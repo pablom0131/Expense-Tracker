b39ef932bdd7463a2af0b0f9be0b6f887c61e88e
Pablo Martinez Castro <pablomc3102@gmail.com>
2023-11-27 16:47:13 -0800

    set on click listeners for buttons on welcome screen

--- app/src/main/java/com/cs4750/team15/expensetracker/MainActivity.kt
+++ app/src/main/java/com/cs4750/team15/expensetracker/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.cs4750.team15.expensetracker
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.expense_list_main)
+        setContentView(R.layout.welcome_screen)
+
+        val expenseListButton = findViewById<Button>(R.id.expense_list_button)
+        val spendingAnalysisButton = findViewById<Button>(R.id.spending_analysis_button)
+        val chatButton = findViewById<Button>(R.id.chat_button)
+
+        expenseListButton.setOnClickListener {
+            setContentView(R.layout.expense_list_main)
+        }
     }
 }
