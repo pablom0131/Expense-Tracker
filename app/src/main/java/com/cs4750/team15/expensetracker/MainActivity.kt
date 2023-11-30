7c0d6a476b30991462c2d1842e06deea370a1119
Pablo Martinez Castro <pablomc3102@gmail.com>
2023-11-29 16:55:27 -0800

    removed listeners for buttons

--- app/src/main/java/com/cs4750/team15/expensetracker/MainActivity.kt
+++ app/src/main/java/com/cs4750/team15/expensetracker/MainActivity.kt
@@ -7,14 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome_screen)
-
-        val expenseListButton = findViewById<Button>(R.id.expense_list_button)
-        val spendingAnalysisButton = findViewById<Button>(R.id.spending_analysis_button)
-        val chatButton = findViewById<Button>(R.id.chat_button)
-
-        expenseListButton.setOnClickListener {
-            setContentView(R.layout.expense_list_main)
-        }
+        setContentView(R.layout.activity_main)
     }
 }
